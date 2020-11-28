6aedfb0513652acf0a052385dea7300e2e996345
Katy <peerasorn14@gmail.com>
2020-11-28 09:59:27 +0700

    [WIP] change folder name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { Scene } from "three";
+// import { Scene } from "three";
 
-const scene = new Scene();
+// const scene = new Scene();
+
+// <a-nft videohandler type='nft'
+//       url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
+//       smooth="true" smoothCount="10" smoothTolerance="0.01" smoothThreshold="5">
+//       <a-video src="#vid" position='50 150 0' width='300' height='175' rotation="0 0 50">
+//       </a-video>
+//     </a-nft>
